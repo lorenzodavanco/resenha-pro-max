d14765c9f4e256a4e51fcd6e316727d2003679cf
lorenzodavanco <lorenzodavanco@hotmail.com>
2026-09-18 12:49:01 -0300

    restart

---
+++ resenhapromax/src/main/java/br/com/uel/resenhapromax/ResenhapromaxApplication.java
@@ -0,0 +1,13 @@
+package br.com.uel.resenhapromax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenhapromaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenhapromaxApplication.class, args);
+	}
+
+}
---
+++ resenhapromax/src/test/java/br/com/uel/resenhapromax/ResenhapromaxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uel.resenhapromax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenhapromaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
